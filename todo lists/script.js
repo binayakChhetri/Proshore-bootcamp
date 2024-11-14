f8e0a6fca98b390f54f30013b71b5ae7bc8507c1
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-11-14 16:18:16 +0545

    Almost complete

--- todo lists/script.js
+++ todo lists/script.js
@@ -24,7 +24,31 @@ const addTodo = () => {
     }
   }
 
+  // Clear input field
+  inputTodo.value = "";
   console.log(todoList);
+  // Add scrollbar
+  if (todoList.length > 9) todoListsField.style.overflowY = "scroll";
+
+  createTodoElements();
+};
+
+const createTodoElements = () => {
+  // Create new todo list elements
+  todoList.forEach((todo, index) => {
+    const todoLi = document.createElement("li");
+    const todoLabel = document.createElement("label");
+    todoLabel.setAttribute("for", `todo-${index}`);
+    todoLabel.textContent = todo;
+    const todoCheckbox = document.createElement("input");
+    todoCheckbox.type = "checkbox";
+    todoCheckbox.id = `todo-${index}`;
+
+    // Append todo list to the todo list field
+    todoLi.appendChild(todoLabel);
+    todoLi.appendChild(todoCheckbox);
+    todoListsField.appendChild(todoLi);
+  });
 };
 
 // Add event listeners
