5ee8fc6dae95997b2f1716db7cac49ed02ba7d36
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-11-15 16:14:39 +0545

    Added features

--- todo lists/script.js
+++ todo lists/script.js
@@ -2,9 +2,37 @@
 const inputTodo = document.querySelector(".input-todo");
 const addBtn = document.querySelector(".add-btn");
 const todoListsField = document.querySelector(".todo-lists");
+let delTodoBtns = document.querySelectorAll(".del-todo");
+let inputCheckboxes = document.querySelectorAll("input[type='checkbox']");
+const sortBtn = document.querySelector("#sort-by");
 
 // Initial todo list value
 let todoList = [];
+
+// Load the todo list from the local storage
+function showTask() {
+  if (localStorage.getItem("todoList")) {
+    todoList = JSON.parse(localStorage.getItem("todoList"));
+    todoList.forEach((todo) => {
+      createTodoList(todo);
+      console.log(todo);
+    });
+
+    update();
+  }
+  // Display scrollbar if todo list is more than 8
+  if (todoList.length > 8) todoListsField.style.overflowY = "scroll";
+
+  if (todoList.length === 0) {
+    const noTodoMsg = document.createElement("li");
+    noTodoMsg.classList.add("no-todo-msg");
+    noTodoMsg.textContent = "No todos found!";
+    todoListsField.appendChild(noTodoMsg);
+  } else {
+    const noTodoMsg = document.querySelector(".no-todo-msg");
+    noTodoMsg.remove();
+  }
+}
 
 // Function to add todo list
 const addTodo = () => {
@@ -15,41 +43,106 @@ const addTodo = () => {
   // First todo is always added
   if (!todoList.length) {
     todoList.push(inputTodo.value);
+    createTodoList(inputTodo.value);
+
+    // Remove no todo message
+    const noTodoMsg = document.querySelector(".no-todo-msg");
+    noTodoMsg.remove();
   } else {
     // Check if the todo activity already exists
     if (!todoList.includes(inputTodo.value)) {
       todoList.push(inputTodo.value);
+      createTodoList(inputTodo.value);
     } else {
       alert("Todo already exists!");
     }
   }
 
-  // Clear input field
-  inputTodo.value = "";
   console.log(todoList);
   // Add scrollbar
   if (todoList.length > 9) todoListsField.style.overflowY = "scroll";
 
-  createTodoElements();
+  // Clear input field
+  inputTodo.value = "";
+  saveData();
+
+  update();
 };
 
-const createTodoElements = () => {
+// Function to create todo list
+const createTodoList = (todo) => {
   // Create new todo list elements
-  todoList.forEach((todo, index) => {
-    const todoLi = document.createElement("li");
-    const todoLabel = document.createElement("label");
-    todoLabel.setAttribute("for", `todo-${index}`);
-    todoLabel.textContent = todo;
-    const todoCheckbox = document.createElement("input");
-    todoCheckbox.type = "checkbox";
-    todoCheckbox.id = `todo-${index}`;
+  const todoLi = document.createElement("li");
+  const todoLabel = document.createElement("label");
+  const todoCheckbox = document.createElement("input");
+  const delTodoBtn = document.createElement("button");
 
-    // Append todo list to the todo list field
-    todoLi.appendChild(todoLabel);
-    todoLi.appendChild(todoCheckbox);
-    todoListsField.appendChild(todoLi);
-  });
+  todoLabel.setAttribute("for", `todo-${todo}`);
+  todoLabel.textContent = todo;
+  todoCheckbox.type = "checkbox";
+  todoCheckbox.id = `todo-${todo}`;
+  delTodoBtn.textContent = "x";
+  delTodoBtn.classList.add("del-todo");
+
+  // Append todo list to the todo list field
+  todoLi.appendChild(todoLabel);
+  todoLi.appendChild(todoCheckbox);
+  todoLi.appendChild(delTodoBtn);
+  todoListsField.appendChild(todoLi);
+};
+
+// Function to remove todo list
+const removeTodo = (e) => {
+  const parentElement = e.target.parentElement;
+  const todo = parentElement.querySelector("label").textContent;
+
+  // Update todoList array
+  todoList = todoList.filter((item) => item !== todo);
+
+  parentElement.remove();
+  saveData();
+
+  // Remove no todo message
+  if (todoList.length === 0) {
+    const noTodoMsg = document.createElement("li");
+    noTodoMsg.classList.add("no-todo-msg");
+    noTodoMsg.textContent = "No todos found!";
+    todoListsField.appendChild(noTodoMsg);
+  }
+
+  // Remove scrollbar if todo list is less than 8
+  if (todoList.length <= 9) todoListsField.style.overflowY = "hidden";
 };
 
 // Add event listeners
 addBtn.addEventListener("click", addTodo);
+inputTodo.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") addTodo();
+});
+
+// Save data in the local storage
+function saveData() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+// Load the todo list from the local storage
+showTask();
+
+// With this function, newly added todo list will be selected and event listener will be added
+function update() {
+  // Select the elements for adding event listener after being loaded
+  delTodoBtns = document.querySelectorAll(".del-todo");
+  delTodoBtns.forEach((delBtn) => {
+    delBtn.addEventListener("click", (e) => removeTodo(e));
+  });
+
+  inputCheckboxes = document.querySelectorAll("input[type='checkbox']");
+  inputCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", (e) => {
+      console.log(e.target.checked);
+      e.target.checked
+        ? (e.target.parentElement.style.color = "#d3d3d3")
+        : (e.target.parentElement.style.color = "#646464");
+    });
+  });
+}
