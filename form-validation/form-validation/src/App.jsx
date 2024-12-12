3ce9e4fdcc85247c31da88fe6f31ae6861223fbe
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-12-12 14:23:49 +0545

    Working with image handling in react

--- form-validation/form-validation/src/App.jsx
+++ form-validation/form-validation/src/App.jsx
@@ -5,6 +5,7 @@ import Signup from "./features/Signup.jsx";
 import Error from "./UI/Error.jsx";
 import { Dashboard } from "./UI/Dashboard.jsx";
 import ProtectedRoute from "./utlis/ProtectedRoute.jsx";
+import { Card } from "./UI/Card.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<Login />} />
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/card" element={<Card />} />
 
           <Route
             path="/dashboard"
