10ff43c1805081f9d262c4cbdf581b4e6aa6b7cf
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-12-15 23:43:23 +0545

    Added mutation to the slice

--- redux-toolkit-api/src/features/api/apiSlice.js
+++ redux-toolkit-api/src/features/api/apiSlice.js
@@ -21,7 +21,34 @@ export const apiSlice = createApi({
     getTodos: builder.query({
       query: () => "/todos",
     }),
+    addTodo: builder.mutation({
+      query: (todo) => ({
+        url: "/todos",
+        method: "POST",
+        body: todo,
+      }),
+    }),
+    updateTodo: builder.mutation({
+      query: (todo) => ({
+        url: `/todos/${todo.id}`,
+        method: "PATCH",
+        body: todo,
+      }),
+    }),
+    deleteTodo: builder.mutation({
+      //Here we destructured the id from the todo object that will be passed in
+      query: ({ id }) => ({
+        url: `/todos/${id}`,
+        method: "DELETE",
+        body: id,
+      }),
+    }),
   }),
 });
 
-export const { useGetTodosQuery } = apiSlice;
+export const {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} = apiSlice;
