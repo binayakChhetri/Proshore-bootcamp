af506eed68a8797ae9b05e3823fe7ee5b4820178
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-12-18 16:44:27 +0545

    Typescript: Learning by very simple project

---
+++ Typescript/restaurant-recommender/index.ts
@@ -0,0 +1,35 @@
+import dataRestaurants from "./dataRestaurants";
+
+const hour: number = new Date().getHours();
+
+const dollarSigns = "$$";
+const deliveryTimeMax = 90;
+const maxDistance = 10;
+
+let result: string;
+
+const priceBracket: number = dollarSigns.length;
+
+const filteredRestaurants = dataRestaurants.filter((restaurant) => {
+  if (Number(restaurant.priceBracket) > priceBracket) {
+    return false;
+  }
+
+  if (restaurant.deliveryTimeMinutes > deliveryTimeMax) {
+    return false;
+  }
+
+  if (Number(restaurant.distance) > maxDistance) {
+    return false;
+  }
+
+  return restaurant;
+});
+
+if (filteredRestaurants.length === 0) {
+  result = "There are no restaurants available right now.";
+} else {
+  result = `We found ${filteredRestaurants.length} restaurants, the first is ${filteredRestaurants[0].name}.`;
+}
+
+console.log(result);
