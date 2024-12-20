45a331e59824ecbb6d80aac545ffe372b6c60026
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-12-21 00:02:05 +0545

    Generic type objects completed

--- Typescript/Functions/index.ts
+++ Typescript/Functions/index.ts
@@ -170,3 +170,66 @@ function mathTutor(operationCallback: OperatorFunction) {
 // Call your functions below:
 mathTutor(multiply);
 // mathTutor(wrongAdd); // will produce an error
+
+// E.) Generic types
+// Generics give us the power to define our own collections of object types.
+// Array<T>
+// This is generic because we can substitute any type (either pre-defined or custom) in the place of T
+
+type MyFamily<T> = {
+  parents: [T, T];
+  mate: T;
+  children: T[];
+};
+
+let myFam: MyFamily<string> = {
+  parents: ["Dad", "Mom"],
+  mate: "Me",
+  children: ["Daughter", "Son"],
+};
+// console.log(myFam);
+
+type Human = { name: string; job: string };
+type Dog = { name: string; tailWagSpeed: number };
+
+type Family<T> = {
+  parents: [T, T];
+  mate: T;
+  children: T[];
+};
+//Do not change the code above this line.
+
+//Provide type annotations for the variables below:
+let theFamily: Family<number> = {
+  parents: [3, 4],
+  mate: 9,
+  children: [5, 30, 121],
+};
+
+let someFamily: Family<boolean> = {
+  parents: [true, true],
+  mate: false,
+  children: [false, false, true, true],
+};
+
+let aFamily: Family<Human> = {
+  parents: [
+    { name: "Mom", job: "software engineer" },
+    { name: "Dad", job: "coding engineer" },
+  ],
+  mate: { name: "Matesky", job: "engineering coder" },
+  children: [{ name: "Babesky", job: "none" }],
+};
+
+let anotherFamily: Family<Dog> = {
+  parents: [
+    { name: "Momo", tailWagSpeed: 3 },
+    { name: "Dado", tailWagSpeed: 100 },
+  ],
+  mate: { name: "Cheems", tailWagSpeed: 7 },
+  children: [
+    { name: "Puppin", tailWagSpeed: 0.001 },
+    { name: "Puppenaut", tailWagSpeed: 0.0001 },
+    { name: "Puppenator", tailWagSpeed: 0.01 },
+  ],
+};
